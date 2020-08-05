cfd86b695d3b9b54ce8433fbd76499be3a091241
astizm <asti-maera@live.nmit.ac.nz>
2020-08-06 09:54:08 +1200

    circle

---
+++ index.js
@@ -0,0 +1,9 @@
+function setup() { //setup code for drawing shapes
+    createCanvas(600, 600); // set canvas size
+}
+
+function draw() { //function for drawing shapes
+    //draw a circle
+    ellipse(300, 300, 250, 250);
+
+}
